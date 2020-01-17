e27cae9d099b2be2c80af12b463f803d22e56569
Satoshi8 <ilkecurryandrice@outlook.jp>
2020-01-17 16:34:25 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/satoshi/takae/calcapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package jp.techacademy.satoshi.takae.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+    }
+    override fun onClick(v: View?){
+        val intent = Intent(this,Main2Activity::class.java)
+        startActivity(intent)
+}
+}
