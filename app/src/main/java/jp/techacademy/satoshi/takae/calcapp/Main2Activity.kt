a34db5ba8a7454a1db6561db32369f4c80a45212
Satoshi8 <ilkecurryandrice@outlook.jp>
2020-01-17 19:39:30 +0900

    修正1

--- app/src/main/java/jp/techacademy/satoshi/takae/calcapp/Main2Activity.kt
+++ app/src/main/java/jp/techacademy/satoshi/takae/calcapp/Main2Activity.kt
@@ -2,11 +2,18 @@ package jp.techacademy.satoshi.takae.calcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val value1 = intent.getIntExtra("VALUE1",0)
+        val value2 = intent.getIntExtra("VALUE2",0)
+
+        textView.text = "${value1 + value2}"
     }
 }
+
--- app/src/main/java/jp/techacademy/satoshi/takae/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/satoshi/takae/calcapp/MainActivity.kt
@@ -5,12 +5,26 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main2.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        override fun onClick(v:View){
+            if (v.id == R.id.button1){
+                
+            }else if (v.id == R.id.button2){
+
+            }else if (v.id == R.id.button3){
+
+            }else if (v.id == R.id.button4){
+
+            }
+        }
+
 
         button1.setOnClickListener(this)
         button2.setOnClickListener(this)
@@ -19,6 +33,9 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onClick(v: View?){
         val intent = Intent(this,Main2Activity::class.java)
+        intent.putExtra("VALUE1",10)
+        intent.putExtra("VALUE2",20)
         startActivity(intent)
+
 }
 }
